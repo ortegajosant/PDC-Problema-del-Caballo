f3bcb8d562a5f959a659266cbbc8c7829026425a
Christian <christianalpizar99@gmail.com>
2019-05-07 22:26:38 -0600

    Se resulve el problema con una solucion, la suncion PDC-sol lista

--- logica_matriz.rkt
+++ logica_matriz.rkt
@@ -1,6 +1,6 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname logica_matriz) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#lang racket
+(provide (all-defined-out))
+
 ;; Elimina un dato de la matriz en una posición específica
 ;; matriz: matriz en trabajo
 ;; fil: fila de la matriz
