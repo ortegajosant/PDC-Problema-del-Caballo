ed2359dc62cdeeb0f15e20e603e2fc5244b2ea8a
AndreyGarro <diego.garro120@gmail.com>
2019-05-10 20:26:24 -0600

    PDC-Test finalizado y documentado, se separa la logica matriz del resto del programa

--- PDC-Sol.rkt
+++ PDC-Sol.rkt
@@ -1,14 +1,29 @@
 #lang racket
 (require "logica_matriz.rkt")
 
+;________________________________________PDC-Sol___________________________________________________
+
+
+
+;________________________________________PDC-Todas_________________________________________________
+
+;Función principal para solucionar el problema
+;tamaño: tamaño de la matriz
+;pos: posición inicial del caballo
+;retorna: una lista con todos los path los cuales llegan a dar una solución.
+(define (PDC-Todas tamano pos)
+  (unirSoluciones tamano (resolverProblema pos tamano (creaMatriz tamano 0)))
+)
+
 ;Llama la función para buscar posibles caminos y elimina los que son nulos.
 ;fila: fila donde se encuentra el caballo.
 ;columna: columna donde se encuentra el caballo.
 ;tamano: tamaño de la matriz.
 ;matriz: matriz lógica encargada de llevar el conteo donde ya ha estado el caballo.
+;flag: se utiliza para saber de que manera se quiere revisar la matriz (al encontrar 0's sea t o f)
 ;retorna: una lista con los posibles caminos reales del caballo.
-(define (buscarPosible fila columna tamano matriz)
-  (EliminarNulos (buscarPosible_aux fila columna tamano matriz))
+(define (buscarPosible fila columna tamano matriz flag)
+  (EliminarNulos (buscarPosible_aux fila columna tamano matriz flag))
 )
 
 ;A partir de una posición busca los posibles caminos que puede tomar el caballo.
@@ -16,24 +31,25 @@
 ;columna: columna donde se encuentra el caballo.
 ;tamano: tamaño de la matriz.
 ;matriz: matriz lógica encargada de llevar el conteo donde ya ha estado el caballo.
+;flag: se utiliza para saber de que manera se quiere revisar la matriz (al encontrar 0's sea t o f)
 ;retorna: una lista con los posibles lugares que el caballo pueda ir.
-(define (buscarPosible_aux fila columna tamano matriz)
+(define (buscarPosible_aux fila columna tamano matriz flag)
      (list
-     (cond ((esValido (- fila 2) (- columna 1) tamano matriz #f) (list (- fila 2) (- columna 1)))
+     (cond ((esValido (- fila 2) (- columna 1) tamano matriz flag) (list (- fila 2) (- columna 1)))
            (else '()))
-     (cond ((esValido (- fila 2) (+ columna 1) tamano matriz #f) (list (- fila 2) (+ columna 1)))
+     (cond ((esValido (- fila 2) (+ columna 1) tamano matriz flag) (list (- fila 2) (+ columna 1)))
            (else '()))
-     (cond ((esValido (- fila 1) (- columna 2) tamano matriz #f) (list (- fila 1) (- columna 2)))
+     (cond ((esValido (- fila 1) (- columna 2) tamano matriz flag) (list (- fila 1) (- columna 2)))
            (else '()))
-     (cond ((esValido (- fila 1) (+ columna 2) tamano matriz #f) (list (- fila 1) (+ columna 2)))
+     (cond ((esValido (- fila 1) (+ columna 2) tamano matriz flag) (list (- fila 1) (+ columna 2)))
            (else '()))
-     (cond ((esValido (+ fila 2) (- columna 1) tamano matriz #f) (list (+ fila 2) (- columna 1)))
+     (cond ((esValido (+ fila 2) (- columna 1) tamano matriz flag) (list (+ fila 2) (- columna 1)))
            (else '()))
-     (cond ((esValido (+ fila 2) (+ columna 1) tamano matriz #f) (list (+ fila 2) (+ columna 1)))
+     (cond ((esValido (+ fila 2) (+ columna 1) tamano matriz flag) (list (+ fila 2) (+ columna 1)))
            (else '()))
-     (cond ((esValido (+ fila 1) (- columna 2) tamano matriz #f) (list (+ fila 1) (- columna 2)))
+     (cond ((esValido (+ fila 1) (- columna 2) tamano matriz flag) (list (+ fila 1) (- columna 2)))
            (else '()))
-     (cond ((esValido (+ fila 1) (+ columna 2) tamano matriz #f) (list (+ fila 1) (+ columna 2)))
+     (cond ((esValido (+ fila 1) (+ columna 2) tamano matriz flag) (list (+ fila 1) (+ columna 2)))
            (else '()))
      )
 )
@@ -59,7 +75,7 @@
 ;tamano: tamaño de la matriz.
 ;matriz: matriz lógica encargada de llevar el conteo donde ya ha estado el caballo.
 (define (numPosibles lista tamano matriz)
-  (numPosibles_aux (buscarPosible (car lista) (cadr lista) tamano matriz))
+  (numPosibles_aux (buscarPosible (car lista) (cadr lista) tamano matriz #f))
 )
 
 ;Funcion auxiliar para llevar el conteo de los posibles caminos.
@@ -75,7 +91,7 @@
 ;tamano: tamaño de la matriz.
 ;matriz: matriz lógica encargada de llevar el conteo donde ya ha estado el caballo.
 (define (resolverProblema pos tamano matriz)
-  (resolverProblema_aux pos (buscarPosible (car pos) (cadr pos) tamano matriz) tamano (insertar matriz 1 (car pos) (cadr pos)))
+  (resolverProblema_aux pos (buscarPosible (car pos) (cadr pos) tamano matriz #f) tamano (insertar matriz 1 (car pos) (cadr pos)))
  )
 
 ;Funcion auxiliar que va creando el path por donde el caballo va pasando y sigue buscando por todas las posiciones.
@@ -111,24 +127,67 @@
     )
   )
 
-;Función principal para solucionar el problema
-;tamaño: tamaño de la matriz
-;pos: posición inicial del caballo
-;retorna: una lista con todos los path los cuales llegan a dar una solución.
-(define (PDC-Todas tamano pos)
-  (unirSoluciones tamano (resolverProblema pos tamano (creaMatriz tamano 0)))
-)
-
 ;;Ejecutar 
-(PDC-Todas 5 '(0 0))
+;(PDC-Todas 5 '(0 0))
 
+;____________________________________PDC-Test____________________________________________________________;
 
+;Función principal
+;tamano: tamaño del tablero.
+;solucion: posible solución.
+;retorna: el recorrido sobre la matriz, si la solución es correcta.
+(define (PDC-Test tamano solucion)
+  (revisaSolucion tamano solucion (creaMatriz tamano 0)))
 
+;Valida si el tamaño de la solución es congruente con el tamaño del tablero,
+;si lo es procede a verificarla.
+;tamano: tamaño del tablero.
+;solucion: posible solucion.
+;matriz: matriz del tamaño indicado, será en la que se irá marcando el camino.
+;retorna: de ser la solucion, la matriz con la ruta.
+(define (revisaSolucion tamano solucion matriz)
+  (cond
+    ((not (= (contarEle solucion) (* tamano tamano)))
+     (write "La ruta dada no recorre o recorre de más todas las casillas del tablero"))
+    (else
+     (revisaSol_aux tamano solucion (creaMatriz tamano 0) 1)
+    )
+   )
+ )
 
+;Revisa que el path dado en la solucion sea correcto, y va editando la matriz.
+;tamano: tamaño del tablero.
+;solucion: posible solucion.
+;matriz: será en la que se irá marcando el camino.
+;cont: es el encargado de ir marcando el orden de las casillas de la matriz.
+;retorna: matriz con la ruta definida.
+(define (revisaSol_aux tamano solucion matriz cont)
+  (cond
+    ((revisaMatriz matriz) matriz)
+    ((seEncuentra (buscarPosible (caar solucion) (cadar solucion) tamano matriz #t) (cdr solucion))
+     (revisaSol_aux tamano (cdr solucion) (insertar matriz cont (caar solucion) (cadar solucion)) (+ cont 1)))
+    (else (write "La solución dada no es correcta"))
+    )
+  )
 
+;Revisa si el siguiente par ordenado de la solucion es un posible y válido movimiento del caballo.
+;listaPosibles: lista con los posibles movimientos del caballo.
+;solucion: este debe estar en la lista de posibles movimientos para que sea cierto.
+;retorna: true o false.
+(define (seEncuentra listaPosibles solucion)
+  (cond
+    ((null? solucion) #t)
+    ((null? listaPosibles) #f)
+    ((and (= (caar listaPosibles) (caar solucion)) (= (cadar listaPosibles) (cadar solucion)) #t))
+    (else
+     (seEncuentra (cdr listaPosibles) solucion))
+    )
+  )
+     
+;Ejecutar 
+;(PDC-Test 5 '((0 0) (2 1) (0 2) (1 0) (3 1) (4 3) (2 2) (1 4) (3 3) (4 1) (2 0) (0 1) (1 3) (3 4) (4 2) (3 0) (1 1) (0 3) (2 4) (1 2) (0 4) (2 3) (4 4) (3 2) (4 0)))
 
-
-
+;______________________________________PDC-Paint_________________________________________________
 
 
 
